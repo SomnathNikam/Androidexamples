5a6787dc2dd67912caa447cbcbb357233f4904c0
SOMNATH <somnathnikam2076@gmail.com>
2023-08-21 02:43:35 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.unitconverter
+
+import android.content.Intent
+import android.os.*
+import androidx.appcompat.app.AppCompatActivity
+
+import android.view.animation.AnimationUtils
+import android.widget.TextView
+import androidx.annotation.RequiresApi
+
+
+class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.P)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val txt = findViewById<TextView>(R.id.mytxt)
+
+            Handler().postDelayed({
+                val a = AnimationUtils.loadAnimation(this, R.anim.alpha)
+                txt.startAnimation(a)
+                val intent = Intent(this,MainActivity2::class.java)
+                startActivity(intent)
+            }, 3000)
+
+    }
+}
